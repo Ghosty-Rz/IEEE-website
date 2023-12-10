67f3ecbde9f37305bb99d361dcb0e0ca6e558012
Rihab Zouitni <zouitnirihab@gmail.com>
2023-12-10 10:49:38 +0100

    Add Portfolio

--- components/ContactInfo.tsx
+++ components/ContactInfo.tsx
@@ -13,6 +13,10 @@ function ContactInfo() {
             
             <a href='https://www.google.com/intl/fr/gmail/about/' target="_blank" rel="noopener noreferrer">
             <h1 className='flex p-2 space-x-12 text-xl font-bold'><MdEmail size={30} className='absolute'/> <span>ieeestudentbranch@aui.ma</span></h1></a>
+        
+            <a href='https://portfolio-asmae.vercel.app/' target="_blank" rel="noopener noreferrer">
+            <h1 className='flex p-2 space-x-4 text-xl font-bold'> <span>The portfolio of one of our co-founder</span></h1></a>
+
         </div>
     </div>
     </div>
